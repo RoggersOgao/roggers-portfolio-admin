d3060b4c66847eaeff8cc78fdd9bdbfe5fbfffd0
RoggersOgao <roggersog@gmail.com>
2023-10-07 20:05:16 +0300

    updated the bug

--- src/dashboardComponents/design/corousel/Corousel.jsx
+++ src/dashboardComponents/design/corousel/Corousel.jsx
@@ -2,34 +2,40 @@
 import React, { useState, useEffect, useContext } from "react";
 import Image from "next/image";
 import styles from "./Corousel.module.scss";
-import {GiBroadheadArrow} from 'react-icons/gi'
+import { GiBroadheadArrow } from "react-icons/gi";
 import DesignContext from "@/dashboardComponents/contexts/designContext/DesignContext";
 import CircularBar from "@/dashboardComponents/spinners/circularSpinner/CircularBar";
-function Corousel({design}) {
-  const {state} = useContext(DesignContext)
-  const [loading, setLoading ] = useState(true)
-  
-  const [index, setIndex] = useState(state.designIndex);
-  const images = design.map((item)=>item.design[0])
-  const description= design.map((item)=>item.description)
+function Corousel({ design }) {
+  const { state } = useContext(DesignContext);
+  const [loading, setLoading] = useState(false);
 
+  const [index, setIndex] = useState(state.designIndex);
+  const images = design.map((item) => item.design[0]);
+  const description = design.map((item) => item.description);
+  const [imageLoading, setImageLoading] = useState(true); 
+
+  useEffect(() => {
+    // Whenever the index changes, set loading to true
+    setLoading(true);
+  }, [index]);
   const handlePrevClick = () => {
     setIndex((prevIndex) => Math.max(prevIndex - 1, 0));
-    setLoading(true)
+    setImageLoading(true); 
   };
 
   const handleNextClick = () => {
     setIndex((prevIndex) => Math.min(prevIndex + 1, images.length - 1));
-    setLoading(true)
+    setImageLoading(true); 
   };
-  // const handleLoadingState = () => {
-  //   setLoading(false)
-  // }
-  console.log(loading)
+  const handleImageLoad = () => {
+    setLoading(false);
+    setImageLoading(false); 
+  };
+  console.log(loading);
   const handleKeyDown = (event) => {
-    if (event.key === 'ArrowLeft') {
+    if (event.key === "ArrowLeft") {
       handlePrevClick();
-    } else if (event.key === 'ArrowRight') {
+    } else if (event.key === "ArrowRight") {
       handleNextClick();
     }
   };
@@ -37,40 +43,38 @@ function Corousel({design}) {
   let currentImage = images[index];
 
   useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener("keydown", handleKeyDown);
     };
   }, [handleKeyDown]);
 
-  
   return (
     <div className={styles.container}>
       <div className={styles.contentLeft}>
         <div className={styles.smCont}>
-        <div className={styles.profile}>
-          <div className={styles.profileLeft}>
-            <Image
-              src="/assets/abstract-colorful-splash-3d-background-generative-ai-background.jpg"
-              alt="abstract-colorful-splash-3d-background-generative-ai-background"
-              width={50}
-              height={50}
-              quality={100}
-              className={styles.img}
-            />
-          </div>
-          <div className={styles.profileRight}>
-            <div className={styles.profileRightTitle}>
-              <h1>{description[index]}</h1>
+          <div className={styles.profile}>
+            <div className={styles.profileLeft}>
+              <Image
+                src="/assets/abstract-colorful-splash-3d-background-generative-ai-background.jpg"
+                alt="abstract-colorful-splash-3d-background-generative-ai-background"
+                width={50}
+                height={50}
+                quality={100}
+                className={styles.img}
+              />
             </div>
-            <div className={styles.name}>
-              <p>Roggers Ogao</p>
+            <div className={styles.profileRight}>
+              <div className={styles.profileRightTitle}>
+                <h1>{description[index]}</h1>
+              </div>
+              <div className={styles.name}>
+                <p>Roggers Ogao</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.imgCont}>
-          
+          <div className={styles.imgCont}>
             <Image
               src={currentImage.secure_url}
               alt={currentImage.original_filename}
@@ -78,38 +82,37 @@ function Corousel({design}) {
               height={1200}
               className={styles.img}
               placeholder="blur"
-              onLoadingComplete={()=>setLoading(false)}
+              onLoadingComplete={handleImageLoad}
               blurDataURL={`data:image/jpeg;base:64,${currentImage.secure_url}`}
-              style={{display: loading ? "none": "block"}}
+              style={{ display: loading ? "none" : "block" }}
             />
-          {loading && (
-            <div className={styles.loader}>
-              <CircularBar />
-              <p>loading image...</p>
-            </div>
-          )}
-          
-        </div>
+            {imageLoading && (
+              <div className={styles.loader}>
+                <CircularBar />
+                <p>loading image...</p>
+              </div>
+            )}
+          </div>
 
-        <div className={styles.carouselNnav}>
-          <button
-            className={styles.prev}
-            onClick={handlePrevClick}
-            disabled={index === 0}
-            style={{display:index === 0 ? "none": "flex"}}
-          >
-            <GiBroadheadArrow className={styles.leftIcon}/>
-          </button>
-          <button
-            className={styles.next}
-            onClick={handleNextClick}
-            disabled={index === images.length - 1}
-            style={{display:index === images.length - 1 ? "none": "flex"}}
-          >
-            <GiBroadheadArrow className={styles.rightIcon}/>
-          </button>
+          <div className={styles.carouselNnav}>
+            <button
+              className={styles.prev}
+              onClick={handlePrevClick}
+              disabled={index === 0}
+              style={{ display: index === 0 ? "none" : "flex" }}
+            >
+              <GiBroadheadArrow className={styles.leftIcon} />
+            </button>
+            <button
+              className={styles.next}
+              onClick={handleNextClick}
+              disabled={index === images.length - 1}
+              style={{ display: index === images.length - 1 ? "none" : "flex" }}
+            >
+              <GiBroadheadArrow className={styles.rightIcon} />
+            </button>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
